57ada4243091ec991f56656265c31ffc1b8b44f0
David <davidmitchellfootball@googlemail.com>
2025-01-18 17:52:20 +0000

    Updating Sermons Page

--- src/components/pages/Sermons.js
+++ src/components/pages/Sermons.js
@@ -1,8 +1,10 @@
+import SermonWidget from "../widgets/SermonWidget";
 function Sermons() {
     return (
         <div className="sermons-page">
             <h1 className="sermons-title">Recent Sermons</h1>
             <p className="sermons-description">Stay inspired by listening to our recent sermons.</p>
+            <SermonWidget />
         </div>
     );
 }
