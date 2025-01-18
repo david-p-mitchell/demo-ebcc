a0e0373cac37650b065a1558904a1ac0cb3aa33e
David <davidmitchellfootball@googlemail.com>
2025-01-18 17:51:57 +0000

    adding sermon Widget

---
+++ src/components/widgets/SermonWidget.js
@@ -0,0 +1,9 @@
+function SermonWidget() {
+    return (
+        <div>
+        <iframe Title="Sermons" src="https://open.spotify.com/embed/show/18dBkVZvm1lsJAwhdJAOY1?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+    );
+}
+
+export default SermonWidget;
