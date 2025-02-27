e8284c57d2d911e0c96aa7bd59d2ce57054a7e96
David <davidmitchellfootball@googlemail.com>
2025-02-27 13:45:38 +0000

    add church History

--- pages/about/church-history.vue
+++ pages/about/church-history.vue
@@ -1,3 +1,17 @@
 <template>
-    <p>PlaceHolder Text</p>
+    <article class="edenPageContent">
+		<h1 class="entry-title">Church History</h1>
+        <div id="post-73" class="article post-73 page type-page status-publish hentry">
+            <h4>
+                <strong>
+                    Brief History of Enon Baptist Church
+                </strong>
+            </h4>
+            <p>A number of people over the years have thought that our church was some new organisation or cult . You will see from this brief account however that Enon is deeply rooted not only in the Bible but in the history of Chatham as well.</p>
+            <p>Enon is a true Bible believing Baptist Church, teaching the same Truths as the early Baptists in England more than three centuries ago. These truths are the same as the teaching of Christ and the Apostles.</p>
+            <p>The Church was formed in 1842 and met for worship in Hamond Hill, just off the New Road. They were joined in 1844 by another group of Baptists who had been meeting down by the river (hence Enon see John chapter 3 verse 23). In 1884 the lease on the building expired and with this in mind a new chapel was built in Nelson Road, now somewhere beneath the Pentagon Shopping Centre.</p>
+            <p>The foundation stone was laid by Mr I. C. Johnson J. P. the man who discovered the formula for making a superior Portland cement and who opened the first cement factory on the Medway at Frindsbury on May 1st 1851.</p>
+            <p>This new chapel was opened for worship on September 27th 1881, and continued as the home of the Church until March 1971, when our present building was completed. This new building, made necessary by the compulsory purchase of the old chapel by the Council, is situated near the Medical Centre between the underpass and the railway line. It is at the junction of three roads, Bryant Street, Claremont Way and Skinner Street.</p>
+        </div>							
+</article>
 </template>
