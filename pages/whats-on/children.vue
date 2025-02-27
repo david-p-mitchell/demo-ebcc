4c2012e1ea173519de96d2adc2678457bdc02a97
David <davidmitchellfootball@googlemail.com>
2025-02-27 13:45:49 +0000

    add sunday club

--- pages/whats-on/children.vue
+++ pages/whats-on/children.vue
@@ -1,3 +1,13 @@
 <template>
-    <p>PlaceHolder Text</p>
+    <article>
+        <img src="https://enonbaptistchatham.org.uk/wp-content/uploads/2017/01/abc.jpg" class="responsive-image" alt="" decoding="async" fetchpriority="high" />
+        <h1>Sunday Club</h1>
+        <p><strong>Every Sunday from 9.45 &#8211; 10.45am.</strong></p>
+        <p>Each week, the children are taught a lesson from the Bible in age groups. We follow a syllabus that covers the main teaching of the Bible over a five-year period.<br />
+        There is also singing, a quiz and if children come regularly they can choose rewards. </p>
+        <p>We go on some trips and have some fun activities for those attending. </p>
+        <p>Lifts can be provided to and from local roads. </p>
+        <p>At a time when morals are often ignored, it is important to consider the spiritual needs of today&#8217;s children.<br />
+        Why not send your children to our Sunday Club? They will be very welcome. </p>
+	</article>
 </template>
