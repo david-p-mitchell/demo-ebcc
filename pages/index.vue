061626815f96a5d172a09a4c79ba76fd2bac0b4e
David <davidmitchellfootball@googlemail.com>
2025-02-28 14:14:22 +0000

    basic home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,17 @@
 <template>
   <div>
-    <ChurchImage @media-loaded="onMediaLoad" />
+    <PageMeta :title="'Home - Enon Baptist Church'" :description="'Welcome to Enon Baptist Church. We are a community of believers committed to worship and service.'" />
+    <ChurchImage />
     <div class="home-page">
       <h1 class="home-title">Welcome to Enon Baptist Church</h1>
-      
       <p class="home-description">
         We are a group of people who meet together to worship God. We are concerned for one another and those who live around us. We believe the Bible to be God’s Word that is relevant to all areas of our life. In particular, we have a personal belief in Jesus Christ and through him have a real relationship with God.
       </p>
-      
     </div>
   </div>
 </template>
 
-<script>
-import ChurchImage from '~/components/ChurchImage'
-
-export default {
-  components: {
-    ChurchImage
-  },
-  data() {
-    return {
-      mediaLoaded: false, // Track the media loading state
-    };
-  },
-  methods: {
-    onMediaLoad() {
-      this.mediaLoaded = true; // Set mediaLoaded to true when media is loaded
-    },
-  },
-}
+<script setup lang="ts">
+import ChurchImage from '~/components/ChurchImage.vue'
 </script>
 
-<style scoped>
-.sermon-widget-desktop {
-    float: right;
-    width: auto;
-    position: flex;
-
-  }
-
-  @media (max-width: 768px) {
-    .sermon-widget-desktop {
-        width: 100%;
-    }
-  }
-</style>
