1582ebaa1032597109cd3135a6a0e722c2c646fc
David <davidmitchellfootball@googlemail.com>
2025-03-17 14:36:20 +0000

    adding Calendar events

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
     <PageMeta :title="'Home - Enon Baptist Church'" :description="'Welcome to Enon Baptist Church. We are a community of believers committed to worship and service.'" />
-    <ChurchImage />
-    <div class="home-page">
-      <h1 class="home-title">Welcome to Enon Baptist Church</h1>
-      <p class="home-description">
-        We are a group of people who meet together to worship God. We are concerned for one another and those who live around us. We believe the Bible to be God’s Word that is relevant to all areas of our life. In particular, we have a personal belief in Jesus Christ and through him have a real relationship with God.
-      </p>
+    <ClientOnly>
+      <ChurchImage @imageRendered="onImageRendered"/>
+    </ClientOnly>
+    <div v-if="imageRendered">
+      <div class="home-page">
+        <h1> Welcome to Enon Baptist Church</h1>
+        <p class="home-description">
+          We are a group of people who meet together to worship God. We are concerned for one another and those who live around us. We believe the Bible to be God’s Word that is relevant to all areas of our life. In particular, we have a personal belief in Jesus Christ and through him have a real relationship with God.
+        </p>
+      </div>
+      <CalendarEventsComponent />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import CalendarEventsComponent from '~/components/events/CalendarEventsComponent.vue';
 import ChurchImage from '~/components/ChurchImage.vue'
+
+import { ref } from 'vue';
+const imageRendered = ref(false);
+
+const onImageRendered = () => {
+  imageRendered.value = true; // Update state
+};
+
+onMounted(() => {
+  imageRendered.value = true;
+});
 </script>
 
