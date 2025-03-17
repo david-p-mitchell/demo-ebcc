b4e4ad7b580943f1cc0fccc424de6ad1fb3dda86
David <davidmitchellfootball@googlemail.com>
2025-03-17 17:12:00 +0000

    add online Prayer Meeting page

--- pages/whats-on/online-prayer-meeting.vue
+++ pages/whats-on/online-prayer-meeting.vue
@@ -1,4 +1,4 @@
 <template>
     <p> PlaceHolder Text</p>
-    <a href = "https://us02web.zoom.us/j/84104984721?pwd=UG5wNGp2eXpDUWtUVmZoRFBPcjVvUT09">Click Here for Zoom Link</a>
+    <a href= "https://us02web.zoom.us/j/84104984721?pwd=UG5wNGp2eXpDUWtUVmZoRFBPcjVvUT09">Click Here for Zoom Link</a>
 </template>
