d224e8cb61f6ed2ada5fa71eeb1dd38b5eb66390
David <davidmitchellfootball@googlemail.com>
2025-04-20 00:29:36 +0100

    change package providers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from 'nuxt/config';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/image'],
+  modules: ['@nuxt/image-edge'],
   alias: {
     '@': './',
   },
