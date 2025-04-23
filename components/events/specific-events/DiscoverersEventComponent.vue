cbcb35fc0831b6ba9f71d732917926dfcc2fe032
David <davidmitchellfootball@googlemail.com>
2025-04-23 01:22:10 +0100

    adding discoverers information

---
+++ components/events/specific-events/DiscoverersEventComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="discoverers">
+        <NuxtLink prefetch to="/whats-on/youth/" class="discoverers box-title">
+    <EventsCalendarEventComponent :event="event" />
+        </NuxtLink>
+    </div>
+</template>
+<script setup lang="ts">
+import type { CalendarEvent } from '~/types/CalendarEvent';
+const props = defineProps<{ event: CalendarEvent }>();
+</script>
+
+<style>
+.discoverers {
+  background-color: #A8DADC;
+  margin-bottom: 1rem;
+  border-radius: 18px;
+  overflow: hidden;
+  text-decoration: none;
+  color: #000;
+}
+.discoverers :hover {
+  background-color:  #7AA8B8 !important;
+  text-decoration: none;
+}
+
+</style>
--- pages/whats-on/youth.vue
+++ pages/whats-on/youth.vue
@@ -1,3 +1,15 @@
 <template>
-    <p>PlaceHolder Text</p>
+    <div>
+        <article>
+            <h1>Discoverers</h1>
+            <div>
+                <p><strong>On every other Friday in (term time) at 7.30 – approx 9.15pm for youth aged 11 (Yr7) – 16: Discoverers</strong></p>
+                <p>Young people of senior school age are welcome to our youth group. Sometimes we meet at the church hall and play games. These games are different each week.</p>
+                <p>There is a Christian message each week and also an opportunity to buy some tuck.</p>
+                <p>We regularly meet up with another youth group and also do things like swimming, tobogganing, and other activities.</p>
+                <p>Why not come and find out what we do?</p>
+                <p>&nbsp;</p>
+            </div>
+        </article>
+    </div>
 </template>
