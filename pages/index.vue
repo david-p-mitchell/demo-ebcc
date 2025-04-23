9719496fcae4ac0957e6478db79f651300307c93
David <davidmitchellfootball@googlemail.com>
2025-04-23 02:46:11 +0100

    rm margin around welcome

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     </ClientOnly>
     <div v-if="imageRendered">
       <div class="home-page">
-        <h1> Welcome to Enon Baptist Church</h1>
+        <h1 class="welcome"> Welcome to Enon Baptist Church</h1>
         <p class="home-description">
           We are a group of people who meet together to worship God. We are concerned for one another and those who live around us. We believe the Bible to be God’s Word that is relevant to all areas of our life. In particular, we have a personal belief in Jesus Christ and through him have a real relationship with God.
         </p>
@@ -30,4 +30,11 @@ const onImageRendered = () => {
 };
 
 </script>
+
+<style setup>
+.welcome {
+  margin-top:0px;
+  margin-bottom:0px;
+}
+</style>
 
