9ebc9313a8d3807dc477c0aad9f1ab34a476a23a
David <davidmitchellfootball@googlemail.com>
2025-05-16 22:26:02 +0100

    change number in GoogleEvents

--- composables/useGoogleEvents.ts
+++ composables/useGoogleEvents.ts
@@ -6,7 +6,7 @@ const events = ref<CalendarEvent[] | null>(null);
 export async function fetchGoogleEvents(): Promise<void> {
   if (events.value !== null) return; // Already fetched
   try {
-    const res = await $fetch<CalendarEvent[]>('/api/google-calendar?number=300');
+    const res = await $fetch<CalendarEvent[]>('/api/google-calendar?number=10');
     events.value = res;
   } catch (error) {
     console.error('Error fetching calendar events:', error);
