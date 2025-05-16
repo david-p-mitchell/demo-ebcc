ca115ad7bd1534bd52ac857896cebc48e4327f04
David <davidmitchellfootball@googlemail.com>
2025-05-16 22:24:05 +0100

    change to layouts

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <NavigationHeader />
+      <main>
+        <div class="content">
+        <slot />
+        </div>
+      </main>
+      <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavigationHeader from '../components/nav/NavigationHeader.vue'
+import Footer from '../components/Footer.vue'
+</script>
+
---
+++ layouts/loading-aware.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-if="!pageReady" class="loader-container">
+      <span class="loader-text">Loading...</span>
+    </div>
+    <div v-else>
+      <NavigationHeader />
+      <main>
+        <div class="content">
+        <slot />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { usePageReady }  from '../composables/usePageReady'
+import NavigationHeader from '../components/nav/NavigationHeader.vue'
+import Footer from '../components/Footer.vue'
+
+const { pageReady } = usePageReady()
+console.log(pageReady.value)
+</script>
+
+<style scoped>
+.loader-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.loader-text {
+  font-size: 1.2rem;
+  color: #777;
+}
+</style>
